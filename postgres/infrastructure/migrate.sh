d9f764b90dc962349cc1956265a261f3a510af42
Hariharan Sivakumar <hariharansivakumar@affordmed.com>
2023-12-02 22:43:00 -0500

    Add exit 0

--- postgres/infrastructure/migrate.sh
+++ postgres/infrastructure/migrate.sh
@@ -15,14 +15,23 @@ for sql_file in "$sql_dir"/*.sql; do
     # Run envsubst
     envsubst < "$sql_file" > "${sql_file}.tmp"
 
+    # Check the exit status of envsubst
+    if [ $? -ne 0 ]; then
+      echo "Unable to substitute variables in $sql_file"
+      exit 1
+    fi
+
     # Rename the temporary file back to the original name
     mv "${sql_file}.tmp" "$sql_file"
   fi
 done
 
-if [ $? -eq 0 ]; then
-    /home/gola/flyway -url=jdbc:postgresql://"${DB_HOST}":"${DB_PORT}"/"${DB_NAME}" -schemas="${DB_NAME}" -user="${DB_USER}" -password="${POSTGRES_PASSWORD}" -connectRetries=60 -mixed=true migrate
-else
-    echo "Unable to substitute credentials"
+# Run flyway migration
+/home/gola/flyway -url=jdbc:postgresql://"${DB_HOST}":"${DB_PORT}"/"${DB_NAME}" -schemas="${DB_NAME}" -user="${DB_USER}" -password="${POSTGRES_PASSWORD}" -connectRetries=60 -mixed=true migrate
+
+if [ $? -ne 0 ]; then
+    echo "Flyway migration failed"
     exit 1
 fi
+
+exit 0
